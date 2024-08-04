8162995ba213d18ae5e916020b00f411380a9dd1
Kairat <dzhumabaev.kai@gmail.com>
2024-08-04 18:22:19 +0600

    fix: field width

--- src/shared/textfield/index.tsx
+++ src/shared/textfield/index.tsx
@@ -1,12 +1,12 @@
 import search_icon from "./assets/search_icon.svg";
 
-function TextField({ width, ...rest }: { width?: string; rest?: any }) {
+function TextField({ classname, ...rest }: { classname: string; rest?: any }) {
   return (
     <div className="relative">
       <img className="absolute left-5 top-3" src={search_icon} />
       <input
         type="text"
-        className={`w-[${width}px] h-10 pl-11 rounded-md`}
+        className={`${classname} h-10 pl-11 rounded-md`}
         {...rest}
       />
     </div>
--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -8,7 +8,7 @@ function Header() {
   return (
     <div className="flex gap-x-6 bg-[#0064EB] py-4 pl-10">
       <img src={github_icon} alt="github" />
-      <TextField width="500" {...keyUp} />
+      <TextField classname="w-125" {...keyUp} />
     </div>
   );
 }
